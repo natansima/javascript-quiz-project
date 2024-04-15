434ff9b9b73fd4f1eb1690219996b60e2e6df7d9
Francisco Soares <franciscosoares13@hotmail.com>
2024-04-15 17:41:36 +0100

    WD Day1 Quiz

--- src/question.js
+++ src/question.js
@@ -1,7 +1,31 @@
-class Question {
+
     // YOUR CODE HERE:
     //
+class Question {
+
+    constructor(text,choices,answer,difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty
+}
+shuffleChoices() {
+    /* for (let i = 0;i<this.choices.length;i++) {
+        const temp = this.choices[i]
+        
+        let random = Math.floor(Math.random() * (i + 1))
+        this.choices[i] = this.choices[random]
+        this.choices[random] = temp                  
+    } */
+    this.choices.sort(() => Math.random() - 0.5);                         
+    console.log(this.choices) 
+}
+
+}
+
+const question1 = new Question("aaa",[0,1,2,3],"ccc","ddd")
+console.log(question1.shuffleChoices());
+
     // 1. constructor (text, choices, answer, difficulty)
 
     // 2. shuffleChoices()
-}
--- src/quiz.js
+++ src/quiz.js
@@ -1,6 +1,40 @@
 class Quiz {
     // YOUR CODE HERE:
-    //
+    constructor(questions, timeLimit,timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex]
+    }
+
+    moveToNextQuestion() {
+        this.currentQuestionIndex ++;
+    }
+
+    shuffleQuestions() {
+        this.questions.sort(() => Math.random() - 0.5)
+    }
+
+    checkAnswer(answer) {
+        this.correctAnswers ++;
+    }
+
+    hasEnded() {
+        if(this.currentQuestionIndex < this.questions.length) {
+            return false
+        }
+        if(this.currentQuestionIndex === this.questions.length) {
+            return true
+        }
+    }
+    
+
+
     // 1. constructor (questions, timeLimit, timeRemaining)
 
     // 2. getQuestion()
