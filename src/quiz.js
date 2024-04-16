e29f742571429af6ede8dca45a1ae33ae7abf600
Francisco Soares <franciscosoares13@hotmail.com>
2024-04-16 17:02:32 +0100

    new

--- src/quiz.js
+++ src/quiz.js
@@ -31,9 +31,26 @@ class Quiz {
         if(this.currentQuestionIndex === this.questions.length) {
             return true
         }
-    }
-    
 
+
+
+    }
+    filterQuestionsByDifficulty(difficulty){
+        if(difficulty>=1 || difficulty<=3) {
+            this.questions = this.questions.filter(question => question.difficulty === difficulty);
+        }
+        
+       
+    }
+
+    averageDifficulty() {
+        const sum = this.questions.reduce((total,
+        question) => {
+            const newTotal = total + question.difficulty
+            return newTotal
+        },0)
+        return sum / this.questions.length;
+    }
 
     // 1. constructor (questions, timeLimit, timeRemaining)
 
